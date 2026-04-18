361f45e4e2b1615e6517869b63775ee894744005
ramune428 <tanjoubitanjoubi@gmail.com>
2026-04-18 22:16:36 +0900

    output を [text_area]から[code_block]に変更

--- app.py
+++ app.py
@@ -4,27 +4,39 @@ from lhz_core import create_piece_from_url
 
 st.title("LHTRPG 駒生成")
 
-piece = ""
+if "piece" not in st.session_state:
+    st.session_state["piece"] = ""
+
+if "character_url" not in st.session_state:
+    st.session_state["character_url"] = ""
 
 with st.form(key="character_form"):
-    character_url = st.text_input("キャラクターURL")
-
-    action_btn_1 = st.form_submit_button("コマンドを生成する")
-    if action_btn_1:
-        try:
-            piece = create_piece_from_url(character_url)
-        except ValueError as e:
-            piece = str(e)
-
-    text_area_1 = st.text_area(
-        "生成結果",
-        value=piece,
-        height=250,
+    character_url = st.text_input(
+        "キャラクターURL",
+        value=st.session_state["character_url"],
     )
 
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col5:
+    col1, col2 = st.columns([4, 1])
+    with col1:
+        action_btn_1 = st.form_submit_button("コマンドを生成する")
+    with col2:
         action_btn_2 = st.form_submit_button("クリア")
 
+    if action_btn_1:
+        st.session_state["character_url"] = character_url
+        try:
+            st.session_state["piece"] = create_piece_from_url(character_url)
+        except ValueError as e:
+            st.session_state["piece"] = str(e)
+
     if action_btn_2:
-        piece = ""
+        st.session_state["piece"] = ""
+        st.session_state["character_url"] = ""
+
+st.caption("生成結果")
+st.code(
+    st.session_state["piece"],
+    language=None,
+    line_numbers=False,
+    wrap_lines=True,
+)
