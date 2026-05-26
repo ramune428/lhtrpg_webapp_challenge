0b4052d2187d90812bd4f6a1c26d57d71d974dc7
ramune428 <tanjoubitanjoubi@gmail.com>
2026-05-26 10:10:52 +0900

    {children}の重複解消

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
       lang="ja"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}
+      <body className="min-h-full flex flex-col">
         {children}
         <Analytics />
       </body>
