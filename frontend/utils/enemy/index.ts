283b6ee008f36bfddb9875775f7e342f3af8ba2e
ramune428 <114979394+ramune428@users.noreply.github.com>
2026-06-02 14:04:38 +0900

    Export enemy form utilities

--- frontend/utils/enemy/index.ts
+++ frontend/utils/enemy/index.ts
@@ -1,6 +1,7 @@
 export * from "./calculate";
 export * from "./constants";
 export * from "./dice";
+export * from "./form";
 export * from "./io";
 export * from "./piece";
 export type * from "./types";
