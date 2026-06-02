c0e8c2593ad47420640177ab9d4201032b44cfca
ramune428 <114979394+ramune428@users.noreply.github.com>
2026-06-03 08:52:25 +0900

    Export enemy preview section component

--- frontend/components/enemy/index.ts
+++ frontend/components/enemy/index.ts
@@ -1,6 +1,7 @@
 export { default as EnemyCalculatedValuesPanel } from "./EnemyCalculatedValuesPanel";
 export { default as EnemyNumberField } from "./EnemyNumberField";
 export { default as EnemyOutputPanel } from "./EnemyOutputPanel";
+export { default as EnemyPreviewSection } from "./EnemyPreviewSection";
 export { default as EnemySection } from "./EnemySection";
 export { default as EnemySelectField } from "./EnemySelectField";
 export { EnemyTextarea, EnemyTextInput } from "./EnemyTextField";
