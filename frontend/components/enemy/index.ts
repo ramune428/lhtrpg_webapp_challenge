c50f5b8a6b50922db7aff71b5a3b1311a4e0562b
ramune428 <114979394+ramune428@users.noreply.github.com>
2026-06-03 09:58:43 +0900

    Export enemy skill section component

--- frontend/components/enemy/index.ts
+++ frontend/components/enemy/index.ts
@@ -5,5 +5,6 @@ export { default as EnemyOutputPanel } from "./EnemyOutputPanel";
 export { default as EnemyPreviewSection } from "./EnemyPreviewSection";
 export { default as EnemySection } from "./EnemySection";
 export { default as EnemySelectField } from "./EnemySelectField";
+export { default as EnemySkillSection } from "./EnemySkillSection";
 export { EnemyTextarea, EnemyTextInput } from "./EnemyTextField";
 export { default as TabButton } from "./TabButton";
